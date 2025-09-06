f77a2340d5aca90c2b1d6d4076ee1ea3c8fd1abf
RayssaBeatriz <rayssa.beatriz1205@gmail.com>
2025-09-05 21:34:28 -0300

    Implementação do bubble sort, busca binaria e busca sequêncial

---
+++ busca_binaria.cpp
@@ -0,0 +1,17 @@
+int BuscaBinaria(int arr[], int n, int x){
+    int esquerda = 0;
+    int direita = n -1;
+
+    while(esquerda <= direita){
+        int meio = (direita - esquerda)/2 + esquerda;
+        if(arr[meio] == x){
+            return meio;
+        }
+        else if(arr[meio] < x){
+            esquerda = meio + 1;
+        }else{
+            direita = meio - 1;
+        }
+    }
+    return -1;
+}
