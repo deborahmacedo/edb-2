4fe004f99f5b20b8402bdb458600d2193480fb28
RayssaBeatriz <rayssa.beatriz1205@gmail.com>
2025-09-06 16:20:31 -0300

    Implementação do insertion sort

---
+++ insertion_sort.cpp
@@ -0,0 +1,12 @@
+void insertionSort(int arr[], int n){
+  for(int i = 1; i < n; i++){
+    int j = i - 1;
+    int chave = arr[i];
+
+    while(j>= 0 && arr[j]> chave){
+      arr[j+1] = arr[j];
+      j--;
+    }
+    arr[j+1] = chave;
+  }
+}
