11f0bd4bfce20e1fd638dbf65a02c928f4642a7d
Júlia Santos <julia.lima.713@ufrn.edu.br>
2025-09-12 11:57:16 -0300

    Cálculo da média do tempo

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <vector>
+#include <chrono>
+#include <random>
 #include "bubblesort.hpp"
 #include "insertionsort.hpp"
 #include "busca_sequencial.hpp"
 #include "busca_binaria.hpp"
-#include <chrono>
-#include <random>
+
 
 void pooularVetorComValoresAleatorios(int arr[], int n, int min, int max) {
     std::random_device rd;
@@ -18,7 +19,6 @@ void pooularVetorComValoresAleatorios(int arr[], int n, int min, int max) {
 }
 
 
-
 int main(){
 
     //POPULAR VETOR
@@ -28,63 +28,100 @@ int main(){
     pooularVetorComValoresAleatorios(vetor, tamanho, 1, 10000); // Valores entre 1 e 10000
     std::cout << "Vetor populado com valores aleatórios." << std::endl;
 
+    std::vector<int> insertion_avarage_times;
+    std::vector<int> bubble_avarage_times;
+    std::vector<int> busca_sequencial_avarage_times;
+    std::vector<int> busca_binaria_avarage_times;
+
+    for(int i = 0; i < 5; ++i) { 
+
+        //INSERTION SORT
+
+        //int arr[] = {64, 34, 25, 12, 22, 11, 90, 1 ,3 ,6 ,8 ,1, 3838, 10203, 203, 123, 456, 789, 100, 5, 78};
+        //int n = sizeof(arr)/sizeof(arr[0]);
+        std::cout << "---------------------------------------------------------------------------------------------------\n";
+        std::cout << "Starting Insertion Sort timing:..." << std::endl;
+        auto start_insertion = std::chrono::high_resolution_clock::now();
+        InsertionSort(vetor, tamanho);
+        auto end_insertion = std::chrono::high_resolution_clock::now();
+        std::cout << "\nFinished Insertion Sort timing:..." << std::endl;
+        auto duration_insertion = std::chrono::duration_cast<std::chrono::milliseconds>(end_insertion - start_insertion);
+
+        std::cout << "\nInsertion Sort execution time: " << duration_insertion.count() << " milliseconds" << std::endl;
+
+        
+        insertion_avarage_times.push_back(duration_insertion.count());
+
+
+
+        //BUBBLE SORT
+        //int arr2[] = {64, 34, 25, 12, 22, 11, 100, 5, 1, 78};
+        //int n2 = sizeof(arr2)/sizeof(arr2[0]);
+        
+        std::cout << "---------------------------------------------------------------------------------------------------\n";
+        std::cout << "\nStarting Bubble Sort timing:..." << std::endl;
+        auto start_bubble = std::chrono::high_resolution_clock::now();
+        BubbleSort(vetor, tamanho);
+        auto end_bubble = std::chrono::high_resolution_clock::now();
+        std::cout << "\nFinished Bubble Sort timing:..." << std::endl;
+        auto duration_bubble = std::chrono::duration_cast<std::chrono::milliseconds>(end_bubble - start_bubble);
+
+        std::cout << "\nBubble Sort execution time: " << duration_bubble.count() << " milliseconds" << std::endl;
     
-    //INSERTION SORT
+        
+        bubble_avarage_times.push_back(duration_bubble.count());
 
-    //int arr[] = {64, 34, 25, 12, 22, 11, 90, 1 ,3 ,6 ,8 ,1, 3838, 10203, 203, 123, 456, 789, 100, 5, 78};
-    //int n = sizeof(arr)/sizeof(arr[0]);
-    
-    std::cout << "Starting Insertion Sort timing:..." << std::endl;
-    auto start_insertion = std::chrono::high_resolution_clock::now();
-    InsertionSort(vetor, tamanho);
-    auto end_insertion = std::chrono::high_resolution_clock::now();
-    std::cout << "\nFinished Insertion Sort timing:..." << std::endl;
-    auto duration_insertion = std::chrono::duration_cast<std::chrono::milliseconds>(end_insertion - start_insertion);
+        //Busca sequencial 
+        //int arr3[] = {2, 3, 4, 10, 40, 65, 34, 76, 54, 65, 99, 8, 5, 4, 3, 21};
+        //int n3 = sizeof(arr3)/sizeof(arr3[0]);
+        std::cout << "---------------------------------------------------------------------------------------------------\n";   
+        int x = 21;
+        std ::cout << "\nStarting Sequential Search timing:..." << std::endl;
+        auto start_busca_sequencial = std::chrono::high_resolution_clock::now();
+        int result = BuscaSequencial(vetor, tamanho, x);
+        std::cout << "\nElement found at position: " << result << std::endl;
+        auto end_busca_sequencial = std::chrono::high_resolution_clock::now();
+        std::cout << "\nFinished Sequential Search timing:..." << std::endl;
+        auto duration_busca_sequencial = std::chrono::duration_cast<std::chrono::milliseconds>(end_busca_sequencial - start_busca_sequencial);
+        std::cout << "\nSequential Search timing: " << duration_busca_sequencial.count() << " milissegundos" << std::endl;
 
-    std::cout << "\nInsertion Sort execution time: " << duration_insertion.count() << " milliseconds" << std::endl;
+        busca_sequencial_avarage_times.push_back(duration_busca_sequencial.count());
 
-    //BUBBLE SORT
-    //int arr2[] = {64, 34, 25, 12, 22, 11, 100, 5, 1, 78};
-    //int n2 = sizeof(arr2)/sizeof(arr2[0]);
-    
-    std::cout << "\nStarting Bubble Sort timing:..." << std::endl;
-    auto start_bubble = std::chrono::high_resolution_clock::now();
-    BubbleSort(vetor, tamanho);
-    auto end_bubble = std::chrono::high_resolution_clock::now();
-    std::cout << "\nFinished Bubble Sort timing:..." << std::endl;
-    auto duration_bubble = std::chrono::duration_cast<std::chrono::milliseconds>(end_bubble - start_bubble);
+        //Busca binária
+        //int arr4[] = {2, 3, 4, 10, 40, 65, 34, 76, 54, 65, 99, 8, 5, 4, 3, 21};
+        //int n4 = sizeof(arr4)/sizeof(arr4[0]);
+        std::cout << "---------------------------------------------------------------------------------------------------\n";
+        int y = 21;
+        InsertionSort(vetor, tamanho); //ordenar 
+        std ::cout << "\nStarting Binary Search timing..." << std::endl;
+        auto start_busca_binaria = std::chrono::high_resolution_clock::now();
+        int result2 = BuscaBinaria(vetor, tamanho, y);
+        std::cout << "\nElement found at position: " << result2 << std::endl;
+        auto end_busca_binaria = std::chrono::high_resolution_clock::now();
+        std::cout << "\nFinished Binary Search timing..." << std::endl;
+        auto duration_busca_binaria = std::chrono::duration_cast<std::chrono::milliseconds>(end_busca_binaria - start_busca_binaria);
+        std::cout << "\nBinary Search execution time: " << duration_busca_binaria.count() << " milliseconds" << std::endl;   
 
-    std::cout << "\nBubble Sort execution time: " << duration_bubble.count() << " milliseconds" << std::endl;
-  
-//Busca sequencial 
-//int arr3[] = {2, 3, 4, 10, 40, 65, 34, 76, 54, 65, 99, 8, 5, 4, 3, 21};
-//int n3 = sizeof(arr3)/sizeof(arr3[0]);
-int x = 21;
-std ::cout << "\nStarting Sequential Search timing:..." << std::endl;
-auto start_busca_sequencial = std::chrono::high_resolution_clock::now();
-int result = BuscaSequencial(vetor, tamanho, x);
-std::cout << "\nElement found at position: " << result << std::endl;
-auto end_busca_sequencial = std::chrono::high_resolution_clock::now();
-std::cout << "\nFinished Sequential Search timing:..." << std::endl;
-auto duration_busca_sequencial = std::chrono::duration_cast<std::chrono::milliseconds>(end_busca_sequencial - start_busca_sequencial);
-std::cout << "\nSequential Search timing: " << duration_busca_sequencial.count() << " milissegundos" << std::endl;
+        busca_binaria_avarage_times.push_back(duration_busca_binaria.count());
+    }
 
-//Busca binária
-//int arr4[] = {2, 3, 4, 10, 40, 65, 34, 76, 54, 65, 99, 8, 5, 4, 3, 21};
-//int n4 = sizeof(arr4)/sizeof(arr4[0]);
-int y = 21;
-InsertionSort(vetor, tamanho); //ordenar 
-std ::cout << "\nStarting Binary Search timing..." << std::endl;
-auto start_busca_binaria = std::chrono::high_resolution_clock::now();
-int result2 = BuscaBinaria(vetor, tamanho, y);
-std::cout << "\nElement found at position: " << result2 << std::endl;
-auto end_busca_binaria = std::chrono::high_resolution_clock::now();
-std::cout << "\nFinished Binary Search timing..." << std::endl;
-auto duration_busca_binaria = std::chrono::duration_cast<std::chrono::milliseconds>(end_busca_binaria - start_busca_binaria);
-std::cout << "\nBinary Search execution time: " << duration_busca_binaria.count() << " milliseconds" << std::endl;   
+    auto insertion_average = 0;
+    auto bubble_average = 0;
+    auto busca_sequencial_average = 0; 
+    auto busca_binaria_average = 0;
 
+    for(int i = 0; i < 5; ++i) { 
+        insertion_average += insertion_avarage_times[i];
+        bubble_average += bubble_avarage_times[i];
+        busca_sequencial_average += busca_sequencial_avarage_times[i];
+        busca_binaria_average += busca_binaria_avarage_times[i];
+    }
 
-
+    std::cout << "---------------------------------------------------------------------------------------------------\n";
+    std::cout << "\nInsertion Sort average time over 5 runs: " << insertion_average / 5 << " milliseconds" << std::endl;
+    std::cout << "\nBubble Sort average time over 5 runs: " << bubble_average / 5 << " milliseconds" << std::endl;
+    std::cout << "\nSequential Search average time over 5 runs: " << busca_sequencial_average / 5 << " milliseconds" << std::endl;
+    std::cout << "\nBinary Search average time over 5 runs: " << busca_binaria_average / 5 << " milliseconds" << std::endl; 
 
     return 0;
 }
